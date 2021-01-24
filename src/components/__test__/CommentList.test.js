08bb90c11ac9b26498b81310e66177e019707a3e
Ivan Mitkov <ivan.mitkov@gmail.com>
2021-01-24 14:28:27 +0200

    Cheerio render

--- src/components/__test__/CommentList.test.js
+++ src/components/__test__/CommentList.test.js
@@ -22,3 +22,18 @@ afterEach(() => {
 it("should create one LI per comment", () => {
   expect(wrapped.find("li").length).toEqual(2);
 });
+it("should show the text for each comment", () => {
+  //Different ways to find li text in wrapped element Enzyme and Cheerio
+  //https://cheerio.js.org/Cheerio.html#text
+  expect(wrapped.render().text()).toContain("comment 1");
+  expect(wrapped.render().text()).toContain("comment 2");
+  expect(wrapped.find("li").first().render().text()).toEqual("comment 1");
+  expect(wrapped.find("li").at(1).render().text()).toEqual("comment 2");
+  expect(wrapped.find("li").last().render().text()).toEqual("comment 2");
+  expect(wrapped.find("ul").childAt(1).last().render().text()).toEqual(
+    "comment 2"
+  );
+  expect(wrapped.find("ul").children().first().render().text()).toEqual(
+    "comment 1"
+  );
+});
